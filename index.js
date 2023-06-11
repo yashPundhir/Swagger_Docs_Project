61337ec9d3316913d8ff9d905b19eef702e2944e
yashPundhir <yash.pundhir.prof@gmail.com>
2023-06-11 22:27:43 +0530

    2 new routes added

--- index.js
+++ index.js
@@ -10,8 +10,38 @@ const swaggerDocument = YAML.load("./swagger.yaml");
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get("/", (req, res) => {
+let courses = [
+	{
+		id: "11",
+		name: "Learn ReactJS",
+		price: 299,
+	},
+	{
+		id: "22",
+		name: "Learn Angular",
+		price: 399,
+	},
+	{
+		id: "33",
+		name: "Learn Django",
+		price: 499,
+	},
+];
+
+app.get("/api/v1/", (req, res) => {
 	res.status(200).send("Home Route");
+});
+
+app.get("/api/v1/lco", (req, res) => {
+	res.status(200).send("hello from lco docs");
+});
+
+app.get("/api/v1/lcoobject", (req, res) => {
+	res.status(200).json({
+		id: "55",
+		name: "Learn Backend",
+		price: 999,
+	});
 });
 
 app.listen(4000, () => {
